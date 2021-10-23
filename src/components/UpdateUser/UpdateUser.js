dd77c1eb92e0337420a6ad68f364c73bb894f666
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-23 08:56:53 +0600

    added updated user

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -10,7 +10,41 @@ const UpdateUser = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => setUser(data));
-  }, []);
+  }, [id]);
+
+  // update user
+  const handleNameChange = (e) => {
+    const updatedName = e.target.value;
+    const updatedUser = { name: updatedName, email: user?.email };
+    setUser(updatedUser);
+  };
+  const handleEmailChange = (e) => {
+    const updatedEmail = e.target.value;
+    // const updatedUser = { ...user };
+    // updatedUser.email = updatedEmail;
+    const updatedUser = { name: user.name, email: updatedEmail };
+    setUser(updatedUser);
+  };
+
+  const handleUpdatedUser = (e) => {
+    const url = `http://localhost:5000/users/${id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          alert("Updated Successfully");
+          setUser({});
+        }
+      });
+
+    e.preventDefault();
+  };
 
   return (
     <div>
@@ -18,6 +52,12 @@ const UpdateUser = () => {
       <p>
         <small> {id} </small>
       </p>
+
+      <form onSubmit={handleUpdatedUser} action="#">
+        <input type="text" onChange={handleNameChange} value={user?.name || ""} />
+        <input type="email" onChange={handleEmailChange} value={user?.email || ""} />
+        <input type="submit" value="Update" />
+      </form>
     </div>
   );
 };
